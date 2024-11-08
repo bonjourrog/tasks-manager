d29e2bfae827ec6e7c238f1d5e83239d8f3cb7c8
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-08 16:05:53 -0700

    refactor: change List entity field type from array of objectIds to array of Task

--- entity/list.entity.go
+++ entity/list.entity.go
@@ -7,11 +7,11 @@ import (
 )
 
 type List struct {
-	ID        primitive.ObjectID    `bson:"_id" json:"_id"`
-	Name      string                `bson:"name" json:"name"`
-	Color     string                `bson:"color" json:"color"`
-	Tasks     *[]primitive.ObjectID `bson:"tasks" json:"tasks"`
-	UserID    string                `bson:"user_id" json:"user_id"`
-	UpdatedAt time.Time             `bson:"updated_at" json:"updated_at"`
-	CreatedAt time.Time             `bson:"created_at" json:"created_at"`
+	ID        primitive.ObjectID `bson:"_id" json:"_id"`
+	Name      string             `bson:"name" json:"name"`
+	Color     string             `bson:"color" json:"color"`
+	Tasks     []Task             `bson:"tasks" json:"tasks"`
+	UserID    string             `bson:"user_id" json:"user_id"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 }
