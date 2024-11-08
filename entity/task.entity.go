7c41183abb719817fb628f459fdc1791ec5c3afb
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-08 16:06:45 -0700

    refactor: change listId field type in Task entity from string to ObjectId

--- entity/task.entity.go
+++ entity/task.entity.go
@@ -12,7 +12,7 @@ type Task struct {
 	Description string             `bson:"description" json:"description"`
 	Done        bool               `bson:"done" json:"done"`
 	UserID      string             `bson:"user_id" json:"user_id"`
-	ListID      string             `bson:"list_id" json:"list_id"`
+	ListID      primitive.ObjectID `bson:"list_id" json:"list_id"`
 	UpdatedAt   time.Time          `bson:"updated_at" json:"updated_at"`
 	CreatedAt   time.Time          `bson:"created_at" json:"created_at"`
 }
