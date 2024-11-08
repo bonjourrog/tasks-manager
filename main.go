c3c0f522cfc3cd39a756874f644a55be34aa8cb3
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-07 17:48:35 -0700

    feat: initial server configuration

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"os"
+
+	"github.com/bonjourrog/taskm/controller"
+	"github.com/bonjourrog/taskm/repository"
+	"github.com/bonjourrog/taskm/routes"
+	"github.com/bonjourrog/taskm/service"
+	"github.com/joho/godotenv"
+)
+
+var (
+	listRepo       repository.ListRepo       = repository.NewListRepository()
+	ListService    service.ListService       = service.NewListService(listRepo)
+	listController controller.ListController = controller.NewListController(ListService)
+
+	httpRouter routes.Router = routes.NewGinRouter()
+)
+
 func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+	httpRouter.POST("/api/list/", listController.Create)
+	httpRouter.SERVE(os.Getenv("PORT"))
 
 }
