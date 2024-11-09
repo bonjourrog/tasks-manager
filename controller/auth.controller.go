be31e2a4646949e2b7e40393f9123a370a0bcbd8
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-09 10:48:56 -0700

    feat: create login function in auth controller

--- controller/auth.controller.go
+++ controller/auth.controller.go
@@ -3,17 +3,20 @@ package controller
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/bonjourrog/taskm/entity"
 	"github.com/bonjourrog/taskm/service"
 	"github.com/bonjourrog/taskm/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type AuthController interface {
 	UserRegister(c *gin.Context)
+	Login(c *gin.Context)
 }
 
 type authController struct{}
@@ -72,6 +75,60 @@ func (*authController) UserRegister(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": mResult.Message,
 		"data":    mResult.InsertedID,
+		"error":   false,
+	})
+}
+func (*authController) Login(c *gin.Context) {
+	var (
+		user    entity.User
+		mResult entity.MongoResult
+		key     = os.Getenv("SECRETE_KEY")
+		t       *jwt.Token
+		s       string
+	)
+	if err := json.NewDecoder(c.Request.Body).Decode(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+			"data":    nil,
+			"error":   true,
+		})
+		return
+	}
+	if user.Email == "" || user.Password == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": "email and password are required",
+			"data":    nil,
+			"error":   true,
+		})
+		return
+	}
+	mResult = _authService.SignIn(user)
+	if !mResult.Success {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": mResult.Message,
+			"data":    nil,
+			"error":   true,
+		})
+		return
+	}
+	expirationTime := time.Now().Add(5 * 24 * time.Hour)
+	t = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"exp":   expirationTime,
+		"user":  user.UserName,
+		"email": user.Email,
+	})
+	s, err := t.SignedString([]byte(key))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+			"data":    nil,
+			"error":   true,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": mResult.Message,
+		"data":    s,
 		"error":   false,
 	})
 }
