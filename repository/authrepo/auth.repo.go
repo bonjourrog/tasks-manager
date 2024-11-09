0c1afcfd93a9e79bb21779ee5e648d5c0287053b
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-09 10:47:31 -0700

    feat: create signIn function in auth repo

--- repository/authrepo/auth.repo.go
+++ repository/authrepo/auth.repo.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/bonjourrog/taskm/db"
 	"github.com/bonjourrog/taskm/entity"
+	"github.com/bonjourrog/taskm/utils"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 type AuthRepo interface {
 	Register(user entity.User) entity.MongoResult
+	SignIn(user entity.User) entity.MongoResult
 }
 
 type authRepo struct{}
@@ -58,6 +60,38 @@ func (*authRepo) Register(user entity.User) entity.MongoResult {
 		Success:    true,
 		Message:    "user registered successfully",
 		InsertedID: fmt.Sprintf("%v", result.InsertedID),
+	}
+	return mResult
+}
+func (*authRepo) SignIn(user entity.User) entity.MongoResult {
+	var (
+		_db          = db.NewMongoConnection()
+		singleResult entity.User
+		mResult      entity.MongoResult
+	)
+	client := _db.Connection()
+	defer func() {
+		client.Disconnect(context.TODO())
+	}()
+	coll := client.Database(os.Getenv("MONGO_DB")).Collection("users")
+	if err := coll.FindOne(context.TODO(), bson.M{"email": user.Email}).Decode(&singleResult); err != nil {
+		mResult = entity.MongoResult{
+			Success: false,
+			Message: "invalid email or password",
+		}
+		return mResult
+	}
+	if err := utils.CheckPassword(singleResult.Password, user.Password); err != nil {
+		mResult = entity.MongoResult{
+			Success: false,
+			Message: "invalid email or password",
+		}
+		return mResult
+	}
+	mResult = entity.MongoResult{
+		Success: true,
+		Message: "authentication successful",
+		Data:    singleResult,
 	}
 	return mResult
 }
