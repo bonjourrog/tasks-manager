f831684aa5a069df38873f4a479d89216a99e911
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-08 19:59:29 -0700

    feat: implement auth/register endpoint for new user registration

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bonjourrog/taskm/controller"
 	"github.com/bonjourrog/taskm/repository"
+	"github.com/bonjourrog/taskm/repository/authrepo"
 	taskrepo "github.com/bonjourrog/taskm/repository/taskRepo"
 	"github.com/bonjourrog/taskm/routes"
 	"github.com/bonjourrog/taskm/service"
@@ -15,6 +16,12 @@ var (
 	taskRepo       taskrepo.Task             = taskrepo.NewTasksRepo()
 	taskService    service.TaskService       = service.NewTaskService(taskRepo)
 	taskController controller.TaskController = controller.NewTaskController(taskService)
+)
+
+var (
+	authRepo       authrepo.AuthRepo         = authrepo.NewAuthRepo()
+	authService    service.AuthService       = service.NewAuthService(authRepo)
+	authController controller.AuthController = controller.NewAuthController(authService)
 )
 
 var (
@@ -29,6 +36,7 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		panic(err)
 	}
+	httpRouter.POST("/api/auth/register/", authController.UserRegister)
 	httpRouter.POST("/api/list/", listController.Create)
 	httpRouter.GET("/api/list/:user_id", listController.GetAll)
 	httpRouter.POST("/api/task/", taskController.Create)
