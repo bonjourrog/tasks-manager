f02751ec7fb58f7929143290315ba9e2fad52c21
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-17 08:36:15 -0700

    feat: create cors middleware to manage access controll settings

---
+++ middleware/cors.middleware.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CorsMiddleware(c *gin.Context) {
+	allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
+	if allowedOrigin != "" {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+	}
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	if c.Request.Method == "OPTIONS" {
+		c.AbortWithStatus(http.StatusNoContent)
+		return
+	}
+	c.Next()
+}
