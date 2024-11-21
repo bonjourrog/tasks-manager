8125a98a13d0c423076d035ae307f066777b74a0
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-21 11:26:21 -0700

    fix: fix the conditional statement when setting the allow origin in cors middleware

--- middleware/cors.middleware.go
+++ middleware/cors.middleware.go
@@ -11,8 +11,9 @@ func CorsMiddleware(c *gin.Context) {
 	allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
 	if allowedOrigin != "" {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+	} else {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	}
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
