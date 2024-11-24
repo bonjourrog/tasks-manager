9b2840cf881f66e5fc65efafbca02297362b93af
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-24 08:51:50 -0700

    feat: create update function in task service

--- service/task.service.go
+++ service/task.service.go
@@ -3,10 +3,12 @@ package service
 import (
 	"github.com/bonjourrog/taskm/entity"
 	"github.com/bonjourrog/taskm/repository/taskrepo"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type TaskService interface {
 	Create(task entity.Task) entity.MongoResult
+	Update(task_id primitive.ObjectID, task entity.Task) entity.MongoResult
 }
 
 type taskSerive struct{}
@@ -22,4 +24,7 @@ func NewTaskService(taskRepo taskrepo.Task) TaskService {
 
 func (*taskSerive) Create(task entity.Task) entity.MongoResult {
 	return _taskRepo.Create(task)
+}
+func (*taskSerive) Update(task_id primitive.ObjectID, task entity.Task) entity.MongoResult {
+	return _taskRepo.UpdateTask(task_id, task)
 }
